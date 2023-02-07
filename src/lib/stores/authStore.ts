5fe4f8fff9c5faf3b1b4c277301b2489b3c26cdc
da7a90-backup <sidbarrack@gmail.com>
2023-02-07 22:00:56 +0000

    Fix: removed unrelated files from commit

--- src/lib/stores/authStore.ts
+++
@@ -1,7 +0,0 @@
-import { env } from '$env/dynamic/public'
-import { browser } from '$app/environment';
-import { writable, type Writable } from 'svelte/store'
-
-export const current_user: Writable<any> = writable(null)
-
-export const user_role: Writable<any> = writable(null)
